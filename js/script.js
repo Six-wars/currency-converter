4d6649354a24619029daabe569c128b620ddc959
Samuel Muiruri <muiruri.samuel@gmail.com>
2018-06-25 18:21:39 +0300

    step

--- js/script.js
+++ js/script.js
@@ -19,7 +19,9 @@ fetch('https://free.currencyconverterapi.com/api/v5/currencies')
 
       //get all the currency ID's
       currencies_ids.push(currency['id']);
-    } return currencies_ids;
+    } 
+    console.log(currencies_ids);
+    return currencies_ids;
   }).then(function(currencies_ids) {
       for (let currency_id of currencies_ids) {
           for (let index in currencies_ids) {
