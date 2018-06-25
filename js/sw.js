cc6959181aa796cf95b0728f3b34dcaa125f527d
Samuel Muiruri <muiruri.samuel@gmail.com>
2018-06-25 13:19:33 +0300

    fix cache path

--- js/sw.js
+++ js/sw.js
@@ -1,11 +1,11 @@
 var cacheName = 'v1';
 var cacheFiles = [
-	'/',
-	'index.html',
-	'css/bootstrap.min.css',
-	'css/style.css',
-	'js/script.js',
-	'img/bootstrap-solid.svg'
+	'./',
+	'./index.html',
+	'./css/bootstrap.min.css',
+	'./css/style.css',
+	'script.js',
+	'./img/bootstrap-solid.svg'
 ]
 
 self.addEventListener('install', function(e) {
