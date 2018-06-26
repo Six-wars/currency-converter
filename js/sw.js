5de0cb8f61acf668b4f678e939381309f739092c
Samuel Muiruri <muiruri.samuel@gmail.com>
2018-06-26 16:09:19 +0300

    trial

--- js/sw.js
+++ js/sw.js
@@ -5,11 +5,11 @@ self.addEventListener('install', function(event) {
 		caches.open('main-cache-v1').then(function(cache) {
 			return cache.addAll(
 					[
-						'../',
+						//'../',
 						'../js/script.js',
-						'../css/style.css',
-						'../css/bootstrap.min.css',
-						'../js/jquery.min.js',
+						//'../css/style.css',
+						//'../css/bootstrap.min.css',
+						//'../js/jquery.min.js',
 						//'../img/bootstrap-solid.svg',
 						//'https://free.currencyconverterapi.com/api/v5/currencies'
 					]
